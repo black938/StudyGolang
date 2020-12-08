0819274308a7f63c767ca8e2c4a6b00d7c9dd3b4
black938 <814215858@qq.com>
2020-12-08 23:44:48 +0800

    add StudyStruct

--- StudyStruct/main.go
+++ StudyStruct/main.go
@@ -1,1 +1,22 @@
-package StudyStruct
+package main
+
+import "fmt"
+
+type Student struct {
+	id   int
+	name string
+}
+type NewInt int  // type of it: main.MyInt 重新整了一个
+type MyInt = int // type of it: int 起别名 并没有起新类型
+type byte uint8
+type rune int32
+
+func main() {
+	xiaoming := Student{id: 1, name: "xiaoming"}
+	fmt.Printf("%#v", xiaoming)
+	type foo struct {
+		id int
+	}
+	_ = foo{id: 1}
+
+}
