be67926f759bc4c76390e692fa914f2b3df845a0
black938 <814215858@qq.com>
2020-12-14 18:33:32 +0800

    go to gin

--- StudyConcurrent/GPM/main.go
+++ StudyConcurrent/GPM/main.go
@@ -6,7 +6,8 @@ import (
 	"time"
 )
 
-// GPM调度 参考https://www.liwenzhou.com/posts/Go/14_concurrence/
+// GPM调度 参考
+//https://www.liwenzhou.com/posts/Go/14_concurrence/
 //一个操作系统线程对应用户态多个goroutine
 //go程序可以同时使用多个操作系统线程
 //goroutine和OS线程是多对多的关系 即m:n
